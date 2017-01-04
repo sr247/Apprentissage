d371e0b035882c67527df0e21af82826e28f83b7
sr247 <stephane.romany@u-psud.fr>
2017-01-04 22:14:11 +0100

    Ajout de différentes focntions + traitement des expressions régulière

--- tp1.py
+++ tp1.py
@@ -1,5 +1,7 @@
 # NEW MODIF
 
+import re
+
 f1 = open("movie_lens.csv")
 f2 = open("movie_lens2.txt")
 f3 = open("movie_lens3.txt")
@@ -8,7 +10,21 @@ def count_ligne(f):
 	c = 0
 	for line in f:
 		c += 1
-	print(c)	
+	print 'Il y a : %d jugements.' % c
+	f.seek(0)
+	
+	
+def count_utilisateur(f):
+	c = 0
+	jugement = []
+	list_user = []	
+	for line in f:		
+		jugement = line.split("|")		
+		if not jugement[0] in list_user:
+			list_user.append(jugement[0])
+
+	
+	print 'Il y a : ' + str(len(list_user)) + ' utilisateurs'
 	f.seek(0)
 	
 def count_film(f):
@@ -20,37 +36,47 @@ def count_film(f):
 		if not jugement[1] in list_film:
 			list_film.append(jugement[1])
 	
-	print(len(list_film))
+	print 'Il y a : ' + str(len(list_film)) + ' films.'
 	old_young_film(list_film)
 	f.seek(0)
 	
 def old_young_film(l):
 	list_date = []
 	date = []	
-	
+	reg_date_exp = re.compile(r"\(([0-9])+\)")
 	for x in l:
 		list_date.append(x.split(" "))
 			
 	for x in list_date:
 		for i in range(len(x)):
-			if ("(" in x[i]) and [0-9] in x[i]:						
-				date.append(x[i])
+			if ("(" in x[i]):
+				if  (re.match(reg_date_exp, x[i])):
+					date.append(x[i])
 
 	date_inf = date_sup = date[0]
 	
 	for x in date:
 		if x > date_sup:
-			date_sup = x		
-		if x <= date_inf:
+			date_sup = x
+		elif x <= date_inf:
 			date_inf = x
-	print 'Le flim le plus ancien est de: ' + date_inf
-	print 'Le flim le plus recent est de: ' + date_sup
+		
+	print 'Le flim le plus ancien est de : %s' % date_inf
+	print 'Le flim le plus recent est de : %s' % date_sup
 	
-	
+# def trace_courbe(f):
 
-#count_ligne(f2)		
-#count_ligne(f3)
+def Analyse(f):
 
-count_film(f3)
-#count_film(f3)
-#count_film(f1)
+	count_ligne(f)
+	count_utilisateur(f)
+	count_film(f)
+
+# count_ligne(f2)		
+# count_ligne(f3)
+
+# count_film(f1)
+# count_film(f3)
+# count_film(f1)
+
+Analyse(f1)
