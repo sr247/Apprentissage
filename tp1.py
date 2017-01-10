48fb6b15fe18d8a791a148a7f373cc8a80a43159
Stephane Romany <sromany@a-018849.ups.u-psud.fr>
2017-01-10 12:42:48 +0100

    Ajout supplémentaire

--- tp1.py
+++ tp1.py
@@ -7,6 +7,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 fichier = open(sys.argv[1])
+user = {}
 
 def count_ligne(f):
 	c = 0
@@ -20,32 +21,59 @@ def sort_jugement(f):
 	
 	jugement = []
 	for line in f:
-		new_field = line.split("|")
-		jugement.append(new_field)
+		if line != "\n":
+			new_field = line.split("|")
+			jugement.append(new_field)
 		
 	# print(jugement)
 	f.seek(0)
 	return jugement
 
 
+
 def count_utilisateur(jugement):
 
-	list_user = []
+	list_user = {}
 	for element in jugement:
-		if not element[0] in list_user:
-			list_user.append(element[0])
-			
-
+		if not list_user.has_key(element[0]):
+			list_user[element[0]] = []
+		
 	print 'Il y a : ' + str(len(list_user)) + ' utilisateurs'
-	#print(list_user)
-	return	list_user
+	sorted(list_user)
+	# print(list_user)
+	return list_user
+
+
+def sort_note(jugement,user):
+	
+	for element in jugement:	
+		element[2].replace(",",'')
+		user[element[0]].append(element[2][0:str.rfind(element[2],r"\d")])				
+		
+	print user		
+	return user	
+	
+		
+def count_note(jugement):
+	
+	list_note = []
+	tmp_note = []
+	for element in jugement:
+		tmp_note.append(element[2][0:str.rfind(element[2],r"\d")])	
+	
+	list_note = tmp_note
+	list_note = [list_note[i].replace(",","") for i in range(len(list_note))]
+	
+	print 'Il y a : ' + str(len(list_note)) + ' notes'
+	return list_note
 
 def count_film(jugement):
 
 	list_film = []
 	for element in jugement:
-		if not element[1] in list_film:
-			list_film.append(element[1])
+		if element:
+			if not element[1] in list_film:
+				list_film.append(element[1])
 	
 	print 'Il y a : ' + str(len(list_film)) + ' films.'
 	#print(list_film)
@@ -77,29 +105,30 @@ def old_young_film(l):
 	print 'Le flim le plus recent est de : %s' % date_sup
 	return list_date
 	
-def sort_note(jugement):
-	
-	list_note = []
-	tmp_note = []
-	for element in jugement:
-		tmp_note.append(element[2][0:str.rfind(element[2],r"\d")])	
-	
-	list_note = tmp_note
-	list_note = [list_note[i].replace(",","") for i in range(len(list_note))]
-	
-	print 'Il y a : ' + str(len(list_note)) + ' notes'
-	print(list_note)
-	return list_note
+
 	
 def trace_courbe(var): # traiter les jugements
 	
-	y = []
-	print 'Tracer de la courbe des notes...'
+	x = np.arange(0,5,1)
+	y = np.arange(0, int(len(var)))
+	distribution = np.arange(5)	
 	
-	x = np.arange(0, int(len(var)))
-	y = x
+	for i in range(len(var)):
+		y[i] = var[i]
+		
+	y.sort()
+	var.sort()
+
+	for i in range(5):
+		distribution[i] = var.count(str(i+1))		
+		
+	print(x)
+	print(y)
+	print(distribution)
 	
-	plt.plot(x,y)			
+	
+	print 'Tracer de la distribution des notes...'
+	plt.bar(x,distribution,1,color='r')			
 	plt.show()
 
 	
@@ -108,10 +137,11 @@ def Analyse(f):
 	# Ajouter ici sort_jugement qui extrait les jugements et stock dans jugement
 	count_ligne(f)
 	jugement = sort_jugement(f)
-	count_utilisateur(jugement)
+	user = count_utilisateur(jugement)
+	user = sort_note(jugement,user)
 	count_film(jugement)
-	note = sort_note(jugement)
-	trace_courbe(note)
+	note = count_note(jugement)
+	#trace_courbe(note)
 
 Analyse(fichier)
 
